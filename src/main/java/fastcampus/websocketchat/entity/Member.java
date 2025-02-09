f6ef20895410d6ae8bed5763f1636f9d2ed2cbf0
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-02-10 04:34:44 +0900

    Feat: kakao 회원가입 기능 연동 (#3)
    * docs: MySQL, Spring Data JPA Dependency
    
    * docs: kakao 동의 항목 설정
    
    * feat: Member Entity
    
    * fix: 동의 내역 인덴트 오류 수정
    
    * feat: Member 엔티티 필드 수정
    
    * feat: DefaultOAuth2UserService 구현체 생성
    
    * feat: kakao 에서 user의 이름을 가져오므로 프론트에서 username 제거
    
    * feat: User의 ID를 Custom 해서 가져오는 기능 구현

---
+++ src/main/java/fastcampus/websocketchat/entity/Member.java
@@ -0,0 +1,44 @@
+package fastcampus.websocketchat.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+    private String nickname;
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private String phoneNumber;
+    private LocalDate birthDay;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+}
