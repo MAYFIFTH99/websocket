ba018f5fbdd82d3a90fb3f86ed675da8170b588b
harvey <alstjr971@naver.com>
2025-02-13 14:46:21 +0900

    feat: OAuth2AuthenticationToken로 캐스팅하던 것을 두 클래스 모두의 상위 클래스인 AbstractAuthenticationToken로 변경

--- src/main/java/fastcampus/websocketchat/controller/StompChatController.java
+++ src/main/java/fastcampus/websocketchat/controller/StompChatController.java
@@ -13,6 +13,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -32,7 +33,7 @@ public class StompChatController {
             @Payload Map<String, String> payload) {
 
         log.info("{} sent {} in {}", principal.getName(), payload, chatroomId);
-        CustomOAuth2User user = (CustomOAuth2User) ((OAuth2AuthenticationToken) principal).getPrincipal();
+        CustomOAuth2User user = (CustomOAuth2User) ((AbstractAuthenticationToken) principal).getPrincipal();
         Message message = chatService.saveMessage(user.getMember(), chatroomId,
                 payload.get("message"));
 
