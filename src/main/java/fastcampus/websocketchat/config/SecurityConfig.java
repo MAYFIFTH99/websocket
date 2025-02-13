92778f9ff70f56a4bb767ab10f401214e3d0ef8e
harvey <alstjr971@naver.com>
2025-02-13 15:17:05 +0900

    feat: 상담사 접속 시 OAuth2 로그인이 아닌 Form 로그인으로 변경

--- src/main/java/fastcampus/websocketchat/config/SecurityConfig.java
+++ src/main/java/fastcampus/websocketchat/config/SecurityConfig.java
@@ -4,13 +4,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(request ->
@@ -23,6 +29,25 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain consultantSecurityFilterChain(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity
+                .securityMatcher("/consultants/**", "consultants","/login")
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(HttpMethod.POST, "/consultants").permitAll()
+                                .anyRequest().hasRole("CONSULTANT"))
+                .formLogin(withDefaults())
+                .csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
